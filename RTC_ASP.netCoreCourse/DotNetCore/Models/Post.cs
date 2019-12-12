c429417be7101acad3edd305efba2f588344113a
TarekSherif <ENG.TAREK.SHERIF@gmail.com>
2019-12-12 13:05:37 +0200

    DataAnnotations

---
+++ RTC_ASP.netCoreCourse/DotNetCore/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCore.Models
+{
+    public class Post
+    {
+        public int id { get; set; }
+
+        [StringLength(10, ErrorMessage = "Do not enter more than 10 characters")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Please enter Email body")]
+        public string body { get; set; }
+        public int UserID { get; set; }
+
+    }
+}
